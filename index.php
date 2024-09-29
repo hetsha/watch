839ccf13cf95308739538067df9f8c3f4c13e727
hetsha <hetshah6315@gmail.com>
2024-09-29 15:00:14 +0530

    done for day 23/9 3pm

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
     <meta name="author" content="Author Name">
     <meta name="keywords" content="Or&euml; Dore, Syze, Bizhuteri, Aksesore, Outlet etc..." />
     <?php include 'include/fav.php' ?>
-    <?php include'include/fav.php'?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
