9a21550af47621b5dedb8c4e6197fec005f752af
hetsha <hetshah6315@gmail.com>
2024-10-02 13:43:35 +0530

    sql

--- ecom_store.sql
+++ ecom_store.sql
@@ -3,23 +3,30 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 01, 2024 at 09:24 PM
+-- Generation Time: Oct 02, 2024 at 10:13 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `ecom_store`
 --
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `admins`
 --
+
 CREATE TABLE `admins` (
   `admin_id` int(10) NOT NULL,
   `admin_name` varchar(255) NOT NULL,
@@ -31,15 +38,20 @@ CREATE TABLE `admins` (
   `admin_job` varchar(255) NOT NULL,
   `admin_about` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Dumping data for table `admins`
 --
+
 INSERT INTO `admins` (`admin_id`, `admin_name`, `admin_email`, `admin_pass`, `admin_image`, `admin_contact`, `admin_country`, `admin_job`, `admin_about`) VALUES
 (1, 'het', 'hetshah6315@gmail.com', 'admin', 'IMG_20230513_122559_265.jpg', '9427961426', 'India', 'Admin', 'founder  ');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `cart`
 --
+
 CREATE TABLE `cart` (
   `cart_id` int(10) NOT NULL,
   `customer_id` int(10) NOT NULL,
@@ -50,43 +62,56 @@ CREATE TABLE `cart` (
   `status` varchar(50) DEFAULT 'active',
   `added_on` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Dumping data for table `cart`
 --
+
 INSERT INTO `cart` (`cart_id`, `customer_id`, `product_id`, `qty`, `p_price`, `size`, `status`, `added_on`) VALUES
-(22, 1, 9, 1, 2120.00, NULL, 'active', '2024-10-01 19:12:11'),
-(23, 1, 7, 1, 566.00, NULL, 'active', '2024-10-01 19:12:14'),
-(29, 2, 7, 2, 566.00, NULL, 'active', '2024-10-01 19:24:01');
+(23, 1, 7, 2, 566.00, NULL, 'active', '2024-10-01 19:12:14'),
+(29, 2, 7, 2, 566.00, NULL, 'active', '2024-10-01 19:24:01'),
+(30, 1, 3, 1, 500.00, NULL, 'active', '2024-10-02 04:58:12');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `categories`
 --
+
 CREATE TABLE `categories` (
   `cat_id` int(10) NOT NULL,
   `cat_title` text NOT NULL,
   `cat_top` text NOT NULL,
   `cat_image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Dumping data for table `categories`
 --
+
 INSERT INTO `categories` (`cat_id`, `cat_title`, `cat_top`, `cat_image`) VALUES
 (1, 'Quantum / watch', 'no', ''),
 (2, 'watch', '', '');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `contact_us`
 --
+
 CREATE TABLE `contact_us` (
   `contact_id` int(10) NOT NULL,
   `contact_email` text NOT NULL,
   `contact_heading` text NOT NULL,
   `contact_desc` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `coupons`
 --
+
 CREATE TABLE `coupons` (
   `coupon_id` int(10) NOT NULL,
   `product_id` int(10) NOT NULL,
@@ -96,10 +121,13 @@ CREATE TABLE `coupons` (
   `coupon_limit` int(100) NOT NULL,
   `coupon_used` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `customers`
 --
+
 CREATE TABLE `customers` (
   `customer_id` int(10) NOT NULL,
   `customer_name` varchar(255) NOT NULL,
@@ -113,16 +141,22 @@ CREATE TABLE `customers` (
   `customer_ip` varchar(255) NOT NULL,
   `customer_confirm_code` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Dumping data for table `customers`
 --
+
 INSERT INTO `customers` (`customer_id`, `customer_name`, `customer_email`, `customer_pass`, `customer_country`, `customer_city`, `customer_contact`, `customer_address`, `customer_image`, `customer_ip`, `customer_confirm_code`) VALUES
 (1, 'het', 'hetshah6315@gmail.com', '123', 'india', '', '9427961426', '', '', '', ''),
-(2, 'heet', 'heetshah6315@gmail.com', '456', 'india', '', '644523458', '', '', '', '');
+(2, 'heet', 'heetshah6315@gmail.com', '456', 'india', '', '644523458', '', '', '', ''),
+(3, 'Rajvi', 'rajvi@gmail.com', '9117', 'India', '', '9408635207', '', '', '', '');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `customer_orders`
 --
+
 CREATE TABLE `customer_orders` (
   `order_id` int(10) NOT NULL,
   `customer_id` int(10) NOT NULL,
@@ -131,37 +165,48 @@ CREATE TABLE `customer_orders` (
   `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `order_status` varchar(255) NOT NULL DEFAULT 'Pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `enquiry_types`
 --
+
 CREATE TABLE `enquiry_types` (
   `enquiry_id` int(10) NOT NULL,
   `enquiry_title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `manufacturers`
 --
+
 CREATE TABLE `manufacturers` (
   `manufacturer_id` int(10) NOT NULL,
   `manufacturer_title` text NOT NULL,
   `manufacturer_top` text NOT NULL,
   `manufacturer_image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Dumping data for table `manufacturers`
 --
+
 INSERT INTO `manufacturers` (`manufacturer_id`, `manufacturer_title`, `manufacturer_top`, `manufacturer_image`) VALUES
 (1, 'ORA', 'no', ''),
 (2, 'Audemars Piguet', 'no', ''),
 (3, 'Breguet', '', ''),
 (4, 'Breitling', '', ''),
 (5, 'Rolex', '', '');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `order_items`
 --
+
 CREATE TABLE `order_items` (
   `order_item_id` int(10) NOT NULL,
   `order_id` int(10) NOT NULL,
@@ -169,10 +214,13 @@ CREATE TABLE `order_items` (
   `qty` int(10) NOT NULL,
   `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `payments`
 --
+
 CREATE TABLE `payments` (
   `payment_id` int(10) NOT NULL,
   `invoice_no` int(10) NOT NULL,
@@ -182,16 +230,21 @@ CREATE TABLE `payments` (
   `code` int(10) NOT NULL,
   `payment_date` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Dumping data for table `payments`
 --
+
 INSERT INTO `payments` (`payment_id`, `invoice_no`, `amount`, `payment_mode`, `ref_no`, `code`, `payment_date`) VALUES
 (1, 1, 5000, 'credit_card', 0, 0, ''),
 (2, 2, 5000, 'paypal', 0, 0, '');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `pending_orders`
 --
+
 CREATE TABLE `pending_orders` (
   `order_id` int(10) NOT NULL,
   `customer_id` int(10) NOT NULL,
@@ -201,16 +254,21 @@ CREATE TABLE `pending_orders` (
   `size` text NOT NULL,
   `order_status` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Dumping data for table `pending_orders`
 --
+
 INSERT INTO `pending_orders` (`order_id`, `customer_id`, `invoice_no`, `product_id`, `qty`, `size`, `order_status`) VALUES
 (1, 1, 0, '1', 1, '', ''),
 (2, 1, 0, '2', 1, '', '');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `products`
 --
+
 CREATE TABLE `products` (
   `product_id` int(10) NOT NULL,
   `p_cat_id` int(10) NOT NULL,
@@ -229,9 +287,11 @@ CREATE TABLE `products` (
   `product_psp_price` decimal(10,2) NOT NULL,
   `status` varchar(50) NOT NULL DEFAULT 'product'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Dumping data for table `products`
 --
+
 INSERT INTO `products` (`product_id`, `p_cat_id`, `cat_id`, `manufacturer_id`, `date`, `product_title`, `product_url`, `product_img1`, `product_img2`, `product_img3`, `product_img4`, `product_img5`, `product_price`, `product_desc`, `product_psp_price`, `status`) VALUES
 (2, 1, 1, 1, '2024-09-30 17:22:23', 'watch', 'ads', '001_08b7b78b.jpg', '001_08b7b78b.jpg', '008_cef4e301.jpg', '001_08b7b78b.jpg', '001_08b7b78b.jpg', 20000.00, '\r\n                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et neque voluptatibus aliquam reprehenderit. Enim est sit aperiam accusantium ab ducimus animi obcaecati, architecto officia qui illum eum odio, amet reprehenderit autem, consequatur praesentium dolorum quidem accusamus expedita cum doloremque vero! Exercitationem, aliquam dicta nam nostrum, fuga, fugiat nihil provident voluptatem in repellat quasi saepe eveniet molestias.', 1220.00, 'product'),
 (3, 2, 1, 2, '2024-09-30 17:31:23', 'hert', 'sdf', '009_6982b6e3.jpg', '010_e78c7f30.jpg', '011_7f5ac369.jpg', '012_3c899e6e.jpg', '002_2497781c.jpg', 1000.00, '', 500.00, 'product'),
@@ -242,27 +302,35 @@ INSERT INTO `products` (`product_id`, `p_cat_id`, `cat_id`, `manufacturer_id`, `
 (8, 3, 2, 2, '2024-09-30 17:53:23', 'test5', 'asd', '009_f0b8f646.jpg', '004_4e254570.jpg', '011_4cdc1c21.jpg', '007_a61f128f.jpg', '006_413ea940.jpg', 6000.00, '', 2200.00, 'product'),
 (9, 3, 2, 5, '2024-09-30 18:25:07', 'test6', 'test7', '008_5fbcab7d.jpg', '010_d40686b8.jpg', '011_8a8b8c13.jpg', '007_0c194eab.jpg', '012_102632b5.jpg', 5652.00, '', 2120.00, 'product'),
 (10, 3, 1, 4, '2024-09-30 18:25:58', 'test8', 'ert', '028_602a8ee5.jpg', '023_5f9b2835.jpg', '025_c55abace.jpg', '030_a13829fa.jpg', '026_3fdbbdc0.jpg', 8956.00, '', 1213.00, 'product');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `product_categories`
 --
+
 CREATE TABLE `product_categories` (
   `p_cat_id` int(10) NOT NULL,
   `p_cat_title` text NOT NULL,
   `p_cat_top` text NOT NULL,
   `p_cat_image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Dumping data for table `product_categories`
 --
+
 INSERT INTO `product_categories` (`p_cat_id`, `p_cat_title`, `p_cat_top`, `p_cat_image`) VALUES
 (1, 'Quantum', 'no', ''),
 (2, 'Rolex', '', ''),
 (3, 'Breguet', '', '');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `store`
 --
+
 CREATE TABLE `store` (
   `store_id` int(10) NOT NULL,
   `store_title` varchar(255) NOT NULL,
@@ -271,23 +339,29 @@ CREATE TABLE `store` (
   `store_button` varchar(255) NOT NULL,
   `store_url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `wishlist`
 --
+
 CREATE TABLE `wishlist` (
   `wishlist_id` int(10) NOT NULL,
   `customer_id` int(10) NOT NULL,
   `product_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Indexes for dumped tables
 --
+
 --
 -- Indexes for table `admins`
 --
 ALTER TABLE `admins`
   ADD PRIMARY KEY (`admin_id`);
+
 --
 -- Indexes for table `cart`
 --
@@ -295,42 +369,50 @@ ALTER TABLE `cart`
   ADD PRIMARY KEY (`cart_id`),
   ADD KEY `customer_id` (`customer_id`),
   ADD KEY `product_id` (`product_id`);
+
 --
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`cat_id`);
+
 --
 -- Indexes for table `contact_us`
 --
 ALTER TABLE `contact_us`
   ADD PRIMARY KEY (`contact_id`);
+
 --
 -- Indexes for table `coupons`
 --
 ALTER TABLE `coupons`
   ADD PRIMARY KEY (`coupon_id`);
+
 --
 -- Indexes for table `customers`
 --
 ALTER TABLE `customers`
   ADD PRIMARY KEY (`customer_id`);
+
 --
 -- Indexes for table `customer_orders`
 --
 ALTER TABLE `customer_orders`
   ADD PRIMARY KEY (`order_id`),
   ADD KEY `customer_id` (`customer_id`);
+
 --
 -- Indexes for table `enquiry_types`
 --
 ALTER TABLE `enquiry_types`
   ADD PRIMARY KEY (`enquiry_id`);
+
 --
 -- Indexes for table `manufacturers`
 --
 ALTER TABLE `manufacturers`
   ADD PRIMARY KEY (`manufacturer_id`);
+
 --
 -- Indexes for table `order_items`
 --
@@ -338,146 +420,175 @@ ALTER TABLE `order_items`
   ADD PRIMARY KEY (`order_item_id`),
   ADD KEY `order_id` (`order_id`),
   ADD KEY `product_id` (`product_id`);
+
 --
 -- Indexes for table `payments`
 --
 ALTER TABLE `payments`
   ADD PRIMARY KEY (`payment_id`);
+
 --
 -- Indexes for table `pending_orders`
 --
 ALTER TABLE `pending_orders`
   ADD PRIMARY KEY (`order_id`);
+
 --
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`product_id`),
   ADD KEY `manufacturer_id` (`manufacturer_id`);
+
 --
 -- Indexes for table `product_categories`
 --
 ALTER TABLE `product_categories`
   ADD PRIMARY KEY (`p_cat_id`);
+
 --
 -- Indexes for table `store`
 --
 ALTER TABLE `store`
   ADD PRIMARY KEY (`store_id`);
+
 --
 -- Indexes for table `wishlist`
 --
 ALTER TABLE `wishlist`
   ADD PRIMARY KEY (`wishlist_id`);
+
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
 --
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
   MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `cart_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
   MODIFY `cat_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `contact_us`
 --
 ALTER TABLE `contact_us`
   MODIFY `contact_id` int(10) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `coupons`
 --
 ALTER TABLE `coupons`
   MODIFY `coupon_id` int(10) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `customer_orders`
 --
 ALTER TABLE `customer_orders`
   MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `enquiry_types`
 --
 ALTER TABLE `enquiry_types`
   MODIFY `enquiry_id` int(10) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `manufacturers`
 --
 ALTER TABLE `manufacturers`
   MODIFY `manufacturer_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
   MODIFY `order_item_id` int(10) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
   MODIFY `payment_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `pending_orders`
 --
 ALTER TABLE `pending_orders`
   MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `product_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
 -- AUTO_INCREMENT for table `product_categories`
 --
 ALTER TABLE `product_categories`
   MODIFY `p_cat_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `store`
 --
 ALTER TABLE `store`
   MODIFY `store_id` int(10) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `wishlist`
 --
 ALTER TABLE `wishlist`
   MODIFY `wishlist_id` int(10) NOT NULL AUTO_INCREMENT;
+
 --
 -- Constraints for dumped tables
 --
+
 --
 -- Constraints for table `cart`
 --
 ALTER TABLE `cart`
   ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
   ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+
 --
 -- Constraints for table `customer_orders`
 --
 ALTER TABLE `customer_orders`
   ADD CONSTRAINT `customer_orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`);
+
 --
 -- Constraints for table `order_items`
 --
 ALTER TABLE `order_items`
   ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `customer_orders` (`order_id`),
   ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+
 --
 -- Constraints for table `products`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`manufacturer_id`);
 COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
